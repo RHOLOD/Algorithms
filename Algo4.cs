7124f5d9b77828fd3bde61fd150766065ac6e0c6
RHOLOD <42797219+RHOLOD@users.noreply.github.com>
2021-10-10 21:13:15 +0500

    Add files via upload

---
+++ Algo4.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+    public class Node<T>
+    {
+        public Node(T data)
+        {
+            Data = data;
+        }
+        public T Data { get; set; }
+        public Node<T> Next { get; set; }
+    }
+    public class Stack<T>
+    {
+        Node<T> head;
+        int count;
+        public Stack() 
+        {
+
+            head = null;
+            head.Next = null;
+            count = 0;
+            // инициализация внутреннего хранилища стека
+        }
+
+        public int Size()
+        {
+            // размер текущего стека		  
+            return count;
+        }
+
+        public T Pop()
+        {
+            if (count > 0)
+            {
+                Node<T> temp = head;
+                head = head.Next; // переустанавливаем верхушку стека на следующий элемент
+                count--;
+                return temp.Data;
+            }
+            return default(T); // null, если стек пустой
+        }
+
+        public void Push(T val)
+        {
+            Node<T> node = new Node<T>(val);
+            node.Next = head; // переустанавливаем верхушку стека на новый элемент
+            head = node;
+            count++;
+        }
+
+        public T Peek()
+        {
+            if (count > 0)
+            {
+                return head.Data;
+            }
+            return default(T); // null, если стек пустой
+        }
+    }
+}
