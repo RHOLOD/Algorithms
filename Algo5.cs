8f4b175d2a854fd0134796c868e96b5311b69bfd
RHOLOD <42797219+RHOLOD@users.noreply.github.com>
2021-10-12 22:59:24 +0500

    Add files via upload

---
+++ Algo5.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+    public class Node<T>
+    {
+        public Node(T data)
+        {
+            Data = data;
+        }
+        public T Data { get; set; }
+        public Node<T> Next { get; set; }
+    }
+    public class Queue<T>
+    {
+        Node<T> head; // головной/первый элемент
+        Node<T> tail; // последний/хвостовой элемент
+        int count;
+        public Queue()
+        {
+            head = null;
+            tail = null;
+            count = 0;
+            // инициализация внутреннего хранилища очереди
+        }
+
+        public void Enqueue(T item)
+        {
+            Node<T> node = new Node<T>(item);
+            Node<T> tempNode = tail;
+            tail = node;
+            if (count == 0)
+                head = tail;
+            else
+                tempNode.Next = tail;
+            count++;
+            // вставка в хвост
+        }
+
+        public T Dequeue()
+        {
+            if (count > 0)
+            {
+                // выдача из головы
+                T queueHead = head.Data;
+                head = head.Next;
+                count--;
+                return queueHead;
+            }
+            else
+            {
+                return default(T); // если очередь пустая
+            }           
+        }
+
+        public int Size()
+        {
+            return count; // размер очереди
+        }
+    }
+}
