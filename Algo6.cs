f8b398476e3994d3cb633fb63e9665c279bd0f94
RHOLOD <42797219+RHOLOD@users.noreply.github.com>
2021-10-16 17:51:38 +0500

    Add files via upload

---
+++ Algo6.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+
+    class Deque<T>
+    {
+        public Deque()
+        {
+            // инициализация внутреннего хранилища
+        }
+
+        public void AddFront(T item)
+        {
+            // добавление в голову
+        }
+
+        public void AddTail(T item)
+        {
+            // добавление в хвост
+        }
+
+        public T RemoveFront()
+        {
+            // удаление из головы
+            return default(T);
+        }
+
+        public T RemoveTail()
+        {
+            // удаление из хвоста
+            return default(T);
+        }
+
+        public int Size()
+        {
+            return 0; // размер очереди
+        }
+    }
+
+}
