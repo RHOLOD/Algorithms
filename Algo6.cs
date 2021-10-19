de3c40738daf68b7ec12513d11ebdf3ee6441c1f
RHOLOD <42797219+RHOLOD@users.noreply.github.com>
2021-10-19 22:37:09 +0500

    Update Algo6.cs

--- Algo6.cs
+++ Algo6.cs
@@ -1,41 +1,106 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace AlgorithmsDataStructures
 {
-
+    public class DoublyNode<T>
+    {
+        public DoublyNode(T data)
+        {
+            Data = data;
+        }
+        public T Data { get; set; }
+        public DoublyNode<T> Previous { get; set; }
+        public DoublyNode<T> Next { get; set; }
+    }
     class Deque<T>
     {
+        DoublyNode<T> head; 
+        DoublyNode<T> tail; 
+        int count;  
         public Deque()
         {
-            // инициализация внутреннего хранилища
+            head = null;
+            tail = null;
+            count = 0;
         }
 
         public void AddFront(T item)
         {
             // добавление в голову
+            DoublyNode<T> node = new DoublyNode<T>(item);
+            DoublyNode<T> temp = head;
+            node.Next = temp;
+            head = node;
+            if (count == 0)
+                tail = head;
+            else
+                temp.Previous = node;
+            count++;
         }
 
         public void AddTail(T item)
         {
             // добавление в хвост
+            DoublyNode<T> node = new DoublyNode<T>(item);
+
+            if (head == null)
+                head = node;
+            else
+            {
+                tail.Next = node;
+                node.Previous = tail;
+            }
+            tail = node;
+            count++;
         }
 
         public T RemoveFront()
         {
             // удаление из головы
-            return default(T);
+            if (count == 0)
+            {
+                return default(T);
+
+            }
+            T outputItem = head.Data;
+            if (count == 1)
+            {
+                head = tail = null;
+            }
+            else
+            {
+                head = head.Next;
+                head.Previous = null;
+            }
+            count--;
+            return outputItem;            
         }
 
         public T RemoveTail()
         {
             // удаление из хвоста
-            return default(T);
+            if (count == 0)
+            {
+                return default(T);
+            }
+            T outputItem = tail.Data;
+            if (count == 1)
+            {
+                head = tail = null;
+            }
+            else
+            {
+                tail = tail.Previous;
+                tail.Next = null;
+            }
+            count--;
+            return outputItem;
         }
 
         public int Size()
         {
-            return 0; // размер очереди
+            return count; // размер очереди
         }
     }
 
